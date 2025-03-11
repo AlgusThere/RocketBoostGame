62a04a1391a440ca5f1d04d3a48c7e7696d42797
Algus <algukenan@gmail.com>
2025-03-11 14:12:16 +0300

    Movement script changed
    -Rotation input

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -4,10 +4,11 @@ using UnityEngine.InputSystem;
 public class Movement : MonoBehaviour
 {
     [SerializeField] InputAction thrust;
+    [SerializeField] InputAction rotation;
+    [SerializeField] float thrustStrength = 100f;
 
     Rigidbody rb;
 
-    [SerializeField] float thrustStrength = 100f;
 
     private void Start()
     {
@@ -17,14 +18,27 @@ public class Movement : MonoBehaviour
     private void OnEnable()
     {
         thrust.Enable();
+        rotation.Enable();
     }
 
     private void FixedUpdate()
+    {
+        ProcessThrust();
+        ProcessRotation();
+    }
+
+    private void ProcessThrust()
     {
         if (thrust.IsPressed())
         {
             rb.AddRelativeForce(Vector3.up * thrustStrength * Time.fixedDeltaTime);
             //Debug.Log("Dang, I need some space");
         }
+    }
+
+    private void ProcessRotation()
+    {
+        float rotationInput = rotation.ReadValue<float>();
+        Debug.Log("here is our rotation value: " + rotationInput);
     }
 }
