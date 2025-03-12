fa2f9a9059eb3f966965addee917b27584b72a33
Algus <algukenan@gmail.com>
2025-03-12 12:48:56 +0300

    Rocket Rotation and Cinemachine
    -Added left and right rotation
    -Added Cinemachine (Position Composer)
    -New methods

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -6,6 +6,7 @@ public class Movement : MonoBehaviour
     [SerializeField] InputAction thrust;
     [SerializeField] InputAction rotation;
     [SerializeField] float thrustStrength = 100f;
+    [SerializeField] float rotationStrength = 100f;
 
     Rigidbody rb;
 
@@ -39,6 +40,19 @@ public class Movement : MonoBehaviour
     private void ProcessRotation()
     {
         float rotationInput = rotation.ReadValue<float>();
-        Debug.Log("here is our rotation value: " + rotationInput);
+        if(rotationInput < 0)
+        {
+            ApplyRotation(rotationStrength);
+        }
+        else if (rotationInput > 0)
+        {
+            ApplyRotation(-rotationStrength);
+        }
+        //Debug.Log("here is our rotation value: " + rotationInput);
+    }
+
+    private void ApplyRotation(float rotationThisFrame)
+    {
+        transform.Rotate(Vector3.forward * rotationThisFrame * Time.fixedDeltaTime);
     }
 }
